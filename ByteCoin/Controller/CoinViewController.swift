d7ecc486c1d312f6c2e8f8792975fbf397733aa7
Artie <artiepolanco@gmail.com>
2020-05-25 22:12:08 -0400

    Removed SSH comment and will try to commit this via SSH

--- ByteCoin/Controller/CoinViewController.swift
+++ ByteCoin/Controller/CoinViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Angela Yu on 11/09/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-// Testing SSH Git from terminal
 import UIKit
 
 class CoinViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
